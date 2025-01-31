4b040be690a97fc2bc3719ca03dc745e2a76f8a2
Marc Mazer <cramrezam@gmail.com>
2025-01-30 22:18:18 -0500

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,34 @@
+-- See https://wezfurlong.org/wezterm/config/files.html
+-- and https://wezfurlong.org/wezterm/config/lua/config/index.html
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Mocha'
+
+config.font = wezterm.font 'Hack Nerd Font Mono'
+config.font_size = 14
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+
+config.keys = {
+  {
+    key = 'r',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ReloadConfiguration,
+  }
+}
+
+config.audible_bell = "Disabled"
+
+-- Start maximized
+wezterm.on('gui-startup', function(cmd)
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+-- and finally, return the configuration to wezterm
+return config
