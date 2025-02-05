1d57f87bd1584297a74237c3e1f0e440a3c0cae6
Marc Mazer <cramrezam@gmail.com>
2025-02-05 11:46:08 -0500

    Update hammerspoon for wezterm and zed editor

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,40 +3,43 @@ local term_app = "alacritty"
 local browser = "chrome"
 local slack = "slack"
 local music = "music"
+local code = "code"
+local zed = "zed"
+local wezterm = "wezterm"
 
 local toggle_app = function(app_name, modifier, keys)
-    hs.hotkey.bind(modifier, keys, function()
-      local app = hs.application.find(app_name)
-      if app then
-          local is_frontmost = app:isFrontmost()
-          if is_frontmost then
-            app:hide()
-          else
-              app:activate()
-          end
+  hs.hotkey.bind(modifier, keys, function()
+    local app = hs.application.find(app_name)
+    if app then
+      local is_frontmost = app:isFrontmost()
+      if is_frontmost then
+        app:hide()
       else
-          hs.application.launchOrFocus(app_name)
+        app:activate()
       end
-    end)
+    else
+      hs.application.launchOrFocus(app_name)
+    end
+  end)
 end
 
-toggle_app(term_app, {"ctrl"}, "\\")
-toggle_app(browser, {"ctrl"}, "`")
-toggle_app(slack, {"ctrl"}, "/")
-toggle_app(music, {"ctrl"}, ".")
+toggle_app(wezterm, { "ctrl" }, "\\")
+toggle_app(browser, { "ctrl" }, ",")
+toggle_app(music, { "ctrl" }, ".")
+toggle_app(zed, { "ctrl" }, ";")
 
-hs.hotkey.bind({"ctrl", "alt"}, "Right", function()
+hs.hotkey.bind({ "ctrl", "alt" }, "Right", function()
   local app = hs.application.frontmostApplication()
   local win = app:focusedWindow()
   win:moveOneScreenEast()
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "Left", function()
+hs.hotkey.bind({ "ctrl", "alt" }, "Left", function()
   local app = hs.application.frontmostApplication()
   local win = app:focusedWindow()
   win:moveOneScreenWest()
 end)
 
-hs.hotkey.bind({"ctrl", "alt"}, "Return", function()
+hs.hotkey.bind({ "ctrl", "alt" }, "Return", function()
   hs.window.focusedWindow():maximize()
 end)
