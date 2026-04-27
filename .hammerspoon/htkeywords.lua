7ac7ac8a91df8450b54a665155b51e31c487464e
Marc Mazer <cramrezam@gmail.com>
2026-04-27 10:14:47 -0400

    update HammerText keywords

--- .hammerspoon/htkeywords.lua
+++ .hammerspoon/htkeywords.lua
@@ -1,10 +1,15 @@
 -- keywords for HammerText expansion
 local keywords = {
     ["xdt"] = function() return os.date("%Y-%m-%d") end,
-    ["xdts"] = function() return os.date("%Y-%m-%dT%H:%M:%S") end,
+    ["xts"] = function() return os.date("%Y-%m-%dT%H:%M:%S") end,
+    ["xtid"] = function() return os.date("%Y%m%d%H%M%S") end,
     ["xutc"] = function() return os.date("!%Y-%m-%dT%H:%M:%S") end,
     ["jdh"] = function() return '# ' .. os.date("%Y-%m-%d") end,
     ["jth"] = function() return '## ' .. os.date("%H:%M") end,
-    ["hh3"] = "###"
+    ["hh1"] = "#",
+    ["hh2"] = "##",
+    ["hh3"] = "###",
+    ["hh4"] = "####",
+    ["hh5"] = '#####'
 }
 return keywords
